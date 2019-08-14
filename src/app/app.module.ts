6589ebdf93271572972c557974e35c8c52e27f5a
Han Jung <Han.Jung@finra.org>
2019-08-14 14:40:23 -0400

    add basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import '../polyfills';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
@@ -17,7 +18,10 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HomeModule } from './home/home.module';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material-module';
 import { MenubarComponent } from './components/menubar/menubar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -25,10 +29,12 @@ export function HttpLoaderFactory(http: HttpClient) {
 }
 
 @NgModule({
-  declarations: [AppComponent, MenubarComponent],
+  declarations: [AppComponent, MenubarComponent, ProfileComponent, WorkspaceComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    MaterialModule,
     HttpClientModule,
     CoreModule,
     SharedModule,
@@ -45,4 +51,4 @@ export function HttpLoaderFactory(http: HttpClient) {
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
